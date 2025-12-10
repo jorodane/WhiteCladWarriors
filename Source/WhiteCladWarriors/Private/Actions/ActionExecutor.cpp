281e9d7375af540cb92258fe637b0ea0e0b60c6f
jorodane <jorodane@naver.com>
2025-12-10 20:02:00 +0900

    Value Map in Executor

--- Source/WhiteCladWarriors/Private/Actions/ActionExecutor.cpp
+++ Source/WhiteCladWarriors/Private/Actions/ActionExecutor.cpp
@@ -4,6 +4,50 @@
 #include "Actions/ActionExecutor.h"
 #include "Actions/ActionNode.h"
 
+void UActionExecutor::SetPosition(const FName& WantTag, const FVector& WantPosition)
+{
+	FVector& Setter = PositionMap.FindOrAdd(WantTag);
+	Setter = WantPosition;
+}
+
+FVector UActionExecutor::GetPosition(const FName& WantTag) const
+{
+	const FVector* Result = PositionMap.Find(WantTag);
+	if (Result) return *Result;
+	else return FVector::ZeroVector;
+}
+
+void UActionExecutor::SetDirection(const FName& WantTag, const FVector& WantDirection)
+{
+	FVector& Setter = DirectionMap.FindOrAdd(WantTag);
+	Setter = WantDirection;
+}
+
+FVector UActionExecutor::GetDirection(const FName& WantTag) const
+{
+	const FVector* Result = DirectionMap.Find(WantTag);
+	if (Result) return *Result;
+	else return FVector::ZeroVector;
+}
+
+void UActionExecutor::AddActor(const FName& WantTag, AActor* WantActor)
+{
+	ActorMultiMap.AddUnique(WantTag, WantActor);
+}
+
+void UActionExecutor::RemoveActor(const FName& WantTag, AActor* WantActor)
+{
+	ActorMultiMap.RemoveSingle(WantTag, WantActor);
+}
+
+TArray<AActor*> UActionExecutor::GetActor(const FName& WantTag) const
+{
+	TArray<AActor*> Result;
+	ActorMultiMap.MultiFind(WantTag, Result);
+	return Result;
+}
+
+
 void UActionExecutor::EnterNode(UActionNode* TargetNode)
 {
 	if (IsValid(TargetNode))
--- Source/WhiteCladWarriors/Public/Actions/ActionExecutor.h
+++ Source/WhiteCladWarriors/Public/Actions/ActionExecutor.h
@@ -28,7 +28,32 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "Action", Meta = (ExposeOnSpawn = "true"))
 	TArray<UUnitActionComponent*> Target;
 
+	TMap<FName, FVector> PositionMap;
+	TMap<FName, FVector> DirectionMap;
+	TMultiMap<FName, AActor*> ActorMultiMap;
+
 public:
+	UFUNCTION(BlueprintCallable, Category = "Action")
+	void SetPosition(const FName& WantTag, const FVector& WantPosition);
+
+	UFUNCTION(BlueprintPure, Category = "Action")
+	FVector GetPosition(const FName& WantTag) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Action")
+	void SetDirection(const FName& WantTag, const FVector& WantDirection);
+
+	UFUNCTION(BlueprintPure, Category = "Action")
+	FVector GetDirection(const FName& WantTag) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Action")
+	void AddActor(const FName& WantTag, AActor* WantActor);
+
+	UFUNCTION(BlueprintCallable, Category = "Action")
+	void RemoveActor(const FName& WantTag, AActor* WantActor);
+
+	UFUNCTION(BlueprintPure, Category = "Action")
+	TArray<AActor*> GetActor(const FName& WantTag) const;
+
 	UFUNCTION(BlueprintCallable, Category = "Action")
 	void EnterNode(UActionNode* TargetNode);
 
