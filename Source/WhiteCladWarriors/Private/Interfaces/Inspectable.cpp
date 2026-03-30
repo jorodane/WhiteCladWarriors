8e83b2fd912d0ba686d7d263a4740f1b9b24694a
jorodane <jorodane@naver.com>
2026-03-30 13:01:15 +0900

    Inspectable Interface

---
+++ Source/WhiteCladWarriors/Private/Interfaces/Inspectable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/Inspectable.h"
+
+// Add default functionality here for any IInspectable functions that are not pure virtual.
---
+++ Source/WhiteCladWarriors/Public/Interfaces/Inspectable.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Inspectable.generated.h"
+
+class UObject;
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UInspectable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class WHITECLADWARRIORS_API IInspectable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Pool")
+	TArray<UObject*> GetInspectorTargets();
+};
