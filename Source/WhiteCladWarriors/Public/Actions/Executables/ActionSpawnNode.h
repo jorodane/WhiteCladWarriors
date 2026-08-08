59e3c4b517a7f8f2212bdaea4b57a1972ffd0e55
jorodane <jorodane@naver.com>
2026-08-08 12:40:26 +0900

    Add Spawn Node System

---
+++ Source/WhiteCladWarriors/Public/Actions/Executables/ActionSpawnNode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actions/Executables/ActionBehaviorNode.h"
+#include "ActionSpawnNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WHITECLADWARRIORS_API UActionSpawnNode : public UActionBehaviorNode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/WhiteCladWarriors/Public/Interfaces/ActionSpawnable.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Generals/Structs/ActionStructures.h"
+#include "ActionSpawnable.generated.h"
+
+class UActionSpawnNode;
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UActionSpawnable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class WHITECLADWARRIORS_API IActionSpawnable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "ActionSpawnable")
+	void SpawnInitialize(UActionSpawnNode* SpawnedNode, const FActionCursorFinder& Cursor);
+	void SpawnInitialize_Implementation(UActionSpawnNode* SpawnedNode, const FActionCursorFinder& Cursor) {}
+};
